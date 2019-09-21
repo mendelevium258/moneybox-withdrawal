2708ec3f26d87d639a1c6698d5c49c5dd51710b1
Will <w.melville.0@gmail.com>
2019-09-21 22:08:03 +0100

    Added additional unit tests using Moq and NUnit

---
+++ src/Moneybox2.test/TransferMoneyTest.cs
@@ -0,0 +1,155 @@
+using Moneybox.App;
+using Moneybox.App.DataAccess;
+using Moneybox.App.Domain.Services;
+using Moneybox.App.Features;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    public class TransferMoneyTests
+    {
+        private Mock<IAccountRepository> accountRepositoryMock;
+        private Mock<INotificationService> notificationServiceMock;
+        private Guid FromAccountGuid;
+        private Guid ToAccountGuid;
+        [SetUp]
+        public void Setup()
+        {
+            accountRepositoryMock = new Mock<IAccountRepository>();
+            notificationServiceMock = new Mock<INotificationService>();
+            FromAccountGuid = new Guid("11111111-1111-1111-1111-111111111111");
+            ToAccountGuid = new Guid("22222222-2222-2222-2222-222222222222");
+        }
+
+
+        [TestCase(1000, 1000, 0, 100, false, false)]
+        [TestCase(3000, 1000, 1000, 3000, true, true)]
+        [TestCase(4000, 1000, 1000, 3000, false, true)]
+        [TestCase(3000, 1000, 0, 3000, true, false)]
+        public void Transfer_Execute_Success(decimal startBalanceFrom, decimal startBalanceTo, decimal paidInTo, decimal amountToTransfer, bool hasNotificationFrom, bool hasNotificationTo)
+        {
+            var emailAddressFrom = "from@example.com";
+            var emailAddressTo = "to@example.com";
+
+            //Arrange
+            accountRepositoryMock.Setup(x => x.GetAccountById(FromAccountGuid)).Returns(new Account
+            {
+                Id = FromAccountGuid,
+                Balance = startBalanceFrom,
+                PaidIn = 0m,
+                Withdrawn = 0m,
+                User = new User()
+                {
+                    Email = emailAddressFrom
+                }
+            });
+            accountRepositoryMock.Setup(x => x.GetAccountById(ToAccountGuid)).Returns(new Account
+            {
+                Id = ToAccountGuid,
+                Balance = startBalanceTo,
+                PaidIn = paidInTo,
+                Withdrawn = 0m,
+                User = new User()
+                {
+                    Email = emailAddressTo
+                }
+            });
+
+            var transferMoney = new TransferMoney(accountRepositoryMock.Object, notificationServiceMock.Object);
+
+            //Act
+            transferMoney.Execute(FromAccountGuid, ToAccountGuid, amountToTransfer);
+
+            //Assert
+            accountRepositoryMock.Verify(x => x.Update(It.IsAny<Account>()), Times.Exactly(2));
+            notificationServiceMock.Verify(x => x.NotifyFundsLow(emailAddressFrom), Times.Exactly(hasNotificationFrom ? 1 : 0));
+            notificationServiceMock.Verify(x => x.NotifyApproachingPayInLimit(emailAddressTo), Times.Exactly(hasNotificationTo ? 1 : 0));
+        }
+        [TestCase(1000, 1000, 0, 1001, false, false)]
+        [TestCase(0, 1000, 0, 1, false, false)]
+        public void Transfer_Execute_NotSufficientFunds(decimal startBalanceFrom, decimal startBalanceTo, decimal paidInTo, decimal amountToTransfer, bool hasNotificationFrom, bool hasNotificationTo)
+        {
+            var emailAddressFrom = "from@example.com";
+            var emailAddressTo = "to@example.com";
+
+            //Arrange
+            accountRepositoryMock.Setup(x => x.GetAccountById(FromAccountGuid)).Returns(new Account
+            {
+                Id = FromAccountGuid,
+                Balance = startBalanceFrom,
+                PaidIn = 0m,
+                Withdrawn = 0m,
+                User = new User()
+                {
+                    Email = emailAddressFrom
+                }
+            });
+            accountRepositoryMock.Setup(x => x.GetAccountById(ToAccountGuid)).Returns(new Account
+            {
+                Id = ToAccountGuid,
+                Balance = startBalanceTo,
+                PaidIn = paidInTo,
+                Withdrawn = 0m,
+                User = new User()
+                {
+                    Email = emailAddressTo
+                }
+            });
+
+            var transferMoney = new TransferMoney(accountRepositoryMock.Object, notificationServiceMock.Object);
+
+            //Act
+            void testDelegate() => transferMoney.Execute(FromAccountGuid, ToAccountGuid, amountToTransfer);
+
+            //Assert
+            Assert.That(testDelegate, Throws.TypeOf<InvalidOperationException>());
+            accountRepositoryMock.Verify(x => x.Update(It.IsAny<Account>()), Times.Never());
+            notificationServiceMock.Verify(x => x.NotifyFundsLow(emailAddressFrom), Times.Never());
+            notificationServiceMock.Verify(x => x.NotifyApproachingPayInLimit(emailAddressTo), Times.Never());
+        }
+        [TestCase(3000, 1000, 3000, 1001, false, false)]
+        [TestCase(1000, 1000, 4000, 1, false, false)]
+        public void Transfer_Execute_PayInLimitExceeded(decimal startBalanceFrom, decimal startBalanceTo, decimal paidInTo, decimal amountToTransfer, bool hasNotificationFrom, bool hasNotificationTo)
+        {
+            var emailAddressFrom = "from@example.com";
+            var emailAddressTo = "to@example.com";
+
+            //Arrange
+            accountRepositoryMock.Setup(x => x.GetAccountById(FromAccountGuid)).Returns(new Account
+            {
+                Id = FromAccountGuid,
+                Balance = startBalanceFrom,
+                PaidIn = 0m,
+                Withdrawn = 0m,
+                User = new User()
+                {
+                    Email = emailAddressFrom
+                }
+            });
+            accountRepositoryMock.Setup(x => x.GetAccountById(ToAccountGuid)).Returns(new Account
+            {
+                Id = ToAccountGuid,
+                Balance = startBalanceTo,
+                PaidIn = paidInTo,
+                Withdrawn = 0m,
+                User = new User()
+                {
+                    Email = emailAddressTo
+                }
+            });
+
+            var transferMoney = new TransferMoney(accountRepositoryMock.Object, notificationServiceMock.Object);
+
+            //Act
+            void testDelegate() => transferMoney.Execute(FromAccountGuid, ToAccountGuid, amountToTransfer);
+
+            //Assert
+            Assert.That(testDelegate, Throws.TypeOf<InvalidOperationException>());
+            accountRepositoryMock.Verify(x => x.Update(It.IsAny<Account>()), Times.Never());
+            notificationServiceMock.Verify(x => x.NotifyFundsLow(emailAddressFrom), Times.Never());
+            notificationServiceMock.Verify(x => x.NotifyApproachingPayInLimit(emailAddressTo), Times.Never());
+        }
+    }
+}
---
+++ src/Moneybox2.test/WithdrawMoneyTest.cs
@@ -0,0 +1,87 @@
+using Moneybox.App;
+using Moneybox.App.DataAccess;
+using Moneybox.App.Domain.Services;
+using Moneybox.App.Features;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    public class WithdrawMoneyTests
+    {
+        private Mock<IAccountRepository> accountRepositoryMock;
+        private Mock<INotificationService> notificationServiceMock;
+        private Guid FromAccountGuid;
+        [SetUp]
+        public void Setup()
+        {
+            accountRepositoryMock = new Mock<IAccountRepository>();
+            notificationServiceMock = new Mock<INotificationService>();
+            FromAccountGuid = new Guid();
+        }
+
+
+        [TestCase(1000, 100, false)]
+        [TestCase(1000, 1000, true)]
+        [TestCase(1000, 500, false)]
+        [TestCase(1000, 501, true)]
+        [TestCase(3000, 3000, true)]
+        [TestCase(4000, 3000, false)]
+        [TestCase(500, 1, true)]
+        public void Withdraw2_Execute_Success(decimal startBalance, decimal amountToWithdraw, bool hasNotification)
+        {
+            var emailAddress = "test@example.com";
+
+            //Arrange
+            accountRepositoryMock.Setup(x => x.GetAccountById(FromAccountGuid)).Returns(new Account
+            {
+                Id = FromAccountGuid,
+                Balance = startBalance,
+                PaidIn = 0m,
+                Withdrawn = 0m,
+                User = new User()
+                {
+                    Email = emailAddress
+                }
+            });
+
+            var withdrawMoney = new WithdrawMoney(accountRepositoryMock.Object, notificationServiceMock.Object);
+
+            //Act
+            withdrawMoney.Execute(FromAccountGuid, amountToWithdraw);
+
+            //Assert
+            accountRepositoryMock.Verify(x => x.Update(It.IsAny<Account>()), Times.Once());
+            notificationServiceMock.Verify(x => x.NotifyFundsLow(emailAddress), Times.Exactly(hasNotification ? 1 : 0));
+        }
+        [TestCase(100, 101)]
+        [TestCase(100, 1000)]
+        public void Withdraw_Execute_NotSufficientFunds(decimal startBalance, decimal amountToWithdraw)
+        {
+            var emailAddress = "test0@example.com";
+
+            //Arrange
+            accountRepositoryMock.Setup(x => x.GetAccountById(FromAccountGuid)).Returns(new Account
+            {
+                Id = FromAccountGuid,
+                Balance = startBalance,
+                Withdrawn = 0m,
+                User = new User
+                {
+                    Email = emailAddress
+                }
+            });
+
+            var withdrawMoney = new WithdrawMoney(accountRepositoryMock.Object, notificationServiceMock.Object);
+
+            //Act
+            void testDelegate() => withdrawMoney.Execute(FromAccountGuid, amountToWithdraw);
+
+            //Assert
+            Assert.That(testDelegate, Throws.TypeOf<InvalidOperationException>());
+            accountRepositoryMock.Verify(x => x.Update(It.IsAny<Account>()), Times.Never());
+            notificationServiceMock.Verify(x => x.NotifyFundsLow(emailAddress), Times.Never());
+        }
+    }
+}
